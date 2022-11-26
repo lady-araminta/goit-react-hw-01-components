6396dc56a5e7a21ea9acbb51839adb528f3a083c
lady-araminta <jparkhomenko@gmail.com>
2022-11-26 13:48:58 +0200

    Task 4

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import { Stat } from './Stat';
 import data from 'data/data.json';
 import { FriendList } from './FriendList';
 import friends from 'data/friends.json';
-// import transactions from 'data/transactions.json';
+import { TransactionHistory } from './TransactionHistory';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
       <Profile user={user} />
       <Stat title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
